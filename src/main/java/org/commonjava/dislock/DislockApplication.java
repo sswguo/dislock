cb0fcaac55fc52519ab6fbd25e714ff4dcf2d801
Wenjie Guo <wguo@redhat.com>
2020-08-07 16:05:33 +0800

    init project

---
+++ src/main/java/org/commonjava/dislock/DislockApplication.java
@@ -0,0 +1,13 @@
+package org.commonjava.dislock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DislockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DislockApplication.class, args);
+	}
+
+}
