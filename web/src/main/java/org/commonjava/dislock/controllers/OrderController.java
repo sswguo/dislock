3b06a8fda4a4d2584d468f68513ad7079ef56c47
Wenjie Guo <wguo@redhat.com>
2020-09-25 15:04:00 +0800

    Add spring redis

--- web/src/main/java/org/commonjava/dislock/controllers/OrderController.java
+++ web/src/main/java/org/commonjava/dislock/controllers/OrderController.java
@@ -2,6 +2,8 @@ package org.commonjava.dislock.controllers;
 
 import org.commonjava.dislock.model.Item;
 import org.commonjava.dislock.model.ItemRepository;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/order")
@@ -18,6 +21,9 @@ public class OrderController {
     @Autowired
     ItemRepository itemRepository;
 
+    @Autowired
+    private RedissonClient redissonClient;
+
     @RequestMapping( method = RequestMethod.GET, value = "/index" )
     public String index() {
         return "Greetings from Spring Boot!";
@@ -26,7 +32,21 @@ public class OrderController {
     @RequestMapping( method = RequestMethod.GET, value = "/items" )
     public List<Item> getAllItems()
     {
-        System.out.println( "find all." );
+        RLock lock = redissonClient.getLock( "Mylock" );
+        lock.lock( );
+        try
+        {
+            //TODO get entry from the db
+            System.out.println( "enter into the lock part." );
+        }
+        catch ( Exception e )
+        {
+            //
+        }
+        finally
+        {
+            lock.unlock();
+        }
         return itemRepository.findAll();
     }
 
